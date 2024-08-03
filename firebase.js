e1a2ce7064e0e9e5439378ef5efc5f87e68b964d
Kashev Jaswal <kjaswal@depaul.edu>
2024-08-03 17:03:35 -0500

    "Changes to data"

--- firebase.js
+++ firebase.js
@@ -7,14 +7,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+require('dotenv').config();
 const firebaseConfig = {
-  apiKey: "AIzaSyCj_mXuK-NLSJuOWqjaDH9pHFrarfYN8sQ",
-  authDomain: "inventory-management-app-9d886.firebaseapp.com",
-  projectId: "inventory-management-app-9d886",
-  storageBucket: "inventory-management-app-9d886.appspot.com",
-  messagingSenderId: "808952719121",
-  appId: "1:808952719121:web:a5907a6d00946a93a5ceb0",
-  measurementId: "G-2EMDKKGQ2W"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
