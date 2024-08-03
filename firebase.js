35f00e4fb937f7e3c4f2286a355e7bccbf55f536
Kashev Jaswal <kjaswal@depaul.edu>
2024-08-03 18:05:18 -0500

    Changes

--- firebase.js
+++ firebase.js
@@ -2,11 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from './firebaseConfig'; 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCj_mXuK-NLSJuOWqjaDH9pHFrarfYN8sQ",
+  authDomain: "inventory-management-app-9d886.firebaseapp.com",
+  projectId: "inventory-management-app-9d886",
+  storageBucket: "inventory-management-app-9d886.appspot.com",
+  messagingSenderId: "808952719121",
+  appId: "1:808952719121:web:a5907a6d00946a93a5ceb0",
+  measurementId: "G-2EMDKKGQ2W"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
